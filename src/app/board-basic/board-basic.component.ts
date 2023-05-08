7b18667d36464b713e82b511b9fc924bf94cefb8
pubuzhixing8 <pubuzhixing@gmail.com>
2023-05-08 17:18:03 +0800

    chore: add demo data

--- src/app/board-basic/board-basic.component.ts
+++ src/app/board-basic/board-basic.component.ts
@@ -12,7 +12,7 @@ import { PlaitElement, PlaitBoardChangeEvent, PlaitBoard } from '@plait/core';
 export class BasicBoardComponent {
   plugins = [withMind];
 
-  value: PlaitElement[] = [];
+  value: PlaitElement[] = demoData;
 
   board!: PlaitBoard;
 
@@ -24,3 +24,39 @@ export class BasicBoardComponent {
     this.board = value;
   }
 }
+
+const demoData = [
+  {
+    type: 'mindmap',
+    id: '2',
+    rightNodeCount: 3,
+    data: { topic: { children: [{ text: '思维导图' }] } },
+    children: [
+      {
+        id: '2-1',
+        data: { topic: { children: [{ text: '富文本' }] } },
+        children: [],
+        width: 42,
+        height: 20,
+      },
+      {
+        id: '2-2',
+        data: { topic: { children: [{ text: '知名脑图产品' }] } },
+        children: [],
+        width: 84,
+        height: 20,
+      },
+      {
+        id: '2-3',
+        data: { topic: { children: [{ text: 'xxxxxxx' }] } },
+        children: [],
+        width: 48,
+        height: 20,
+      },
+    ],
+    width: 72,
+    height: 25,
+    isRoot: true,
+    points: [[560, 700]],
+  },
+] as PlaitElement[];
